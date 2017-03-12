6100b9e4bdd91a6af308047f5a0c78fd54a32d03
sagarsachdeva <sachdevs@tcd.ie>
2017-03-12 13:23:00 +0000

    Update MyClient.java

--- src/main/java/com/snmp/example/MyClient.java
+++ src/main/java/com/snmp/example/MyClient.java
@@ -7,16 +7,7 @@ public class MyClient {
 	public static void main(String args[]) {
 		try {
 			SimpleSnmpClient client = new SimpleSnmpClient("udp:127.0.0.1/2001");
-			String sysDescr = client.getAsString(new OID(".1.3.7.1.2.1.2.1.1"));
-			
-			//Db Info
-			System.out.println(client.getAsString(new OID(".1.3.7.1.2.1.2.1.1")));
-			System.out.println(client.getAsString(new OID(".1.3.7.1.2.1.2.2.1")));
-			System.out.println(client.getAsString(new OID(".1.3.7.1.2.1.2.3.1")));
-			System.out.println(client.getAsString(new OID(".1.3.7.1.2.1.2.4.1")));
-			System.out.println(client.getAsString(new OID(".1.3.7.1.2.1.2.5.1")));
-			System.out.println(client.getAsString(new OID(".1.3.7.1.2.1.2.6.1")));
-			System.out.println(client.getAsString(new OID(".1.3.7.1.2.1.2.7.1")));
+			String sysDescr = client.getAsString(new OID(".1.3.6.1.2.1.1.1.0"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
